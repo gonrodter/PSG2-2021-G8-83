b5a4c08ad6e380891bc8e54e095a250e3cbc5715
Lucia <72704039+luctorgom@users.noreply.github.com>
2021-04-04 16:03:09 +0200

    Update PetRepository.java
    Borrando métodos que ya no sirven

--- src/main/java/org/springframework/samples/petclinic/repository/PetRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/PetRepository.java
@@ -83,12 +83,6 @@ public interface PetRepository extends Repository<Pet, Integer>, CrudRepository<
 	@Modifying
 	@Query("delete FROM Visit where pet.id =:petId")
 	public void deleteAllVisit(@Param("petId") int petId); 
-		
-	/***
-	@Transactional
-	@Modifying
-	@Query("delete FROM Booking where pet.id =:petId")
-	public void deleteAllBooking(@Param("petId") int petId);
-	**/
+
 
 }
